2366519302dc5ee97988133302bf4bdb4e128f6d
hyuding <qo0723@naver.com>
2025-08-27 09:03:46 +0900

    solved

--- class2/solved_1874.py
+++ class2/solved_1874.py
@@ -2,11 +2,43 @@ n = int(input())
 
 arr = [int(input()) for _ in range(n)]
 # [4, 3, 6, 8, 7, 5, 2, 1]
-stack = []
+stack, result_list, result_calc = [], [], []
 
-for i in range(n):
-    if stack:
-        if arr[i] > stack[-1]:
-            pass
+def push(num):
+    stack.append(num)
+    result_calc.append('+')
+
+def pop():
+    result_list.append(stack.pop())
+    result_calc.append('-')
+
+count = 1
+i = 0
+result = 0
+while True:
+    
+    # pop
+    if arr[i] < count:
+        pop()
+        i += 1    
+    
     else:
-        stack.append((arr[i]))
+        # push
+        push(count)
+        count += 1
+
+    if count > n and len(stack) == 0:
+
+        if result_list == arr:
+            result = True
+            break
+        else:
+            result = False
+            break
+
+if result:
+    for a in result_calc:
+        print(a)
+else:   
+    print('NO')
+
