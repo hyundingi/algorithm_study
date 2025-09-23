9e3b3c6d41e19cae082ba1fc625a1039b1d6f3e8
hyuding <qo0723@naver.com>
2025-09-23 17:55:03 +0900

    study

--- 3gorithm/solved_1937.py
+++ 3gorithm/solved_1937.py
@@ -1,26 +1,43 @@
+from collections import deque
+
 # 상 하 좌 우
 dys = [-1, 1, 0, 0]
 dxs = [0, 0, -1, 1]
 
-def recur():
+def bfs(y, x):
+    visited = [[0] * n for _ in range(n)]
+    queue = deque()
+    queue.append((y,x))
+    cnt = 0
+    while queue:
+        y, x = queue.popleft()
+        visited[y][x] = 1
 
-    for y in range(n):
-        for x in range(n):
-            if visited[y][x]:
-                continue
+        for d in range(4):
+            ny = y + dys[d]
+            nx = x + dxs[d]
 
-            for d in range(4):
-                if 0 <= y + dys[d] < n and 0 <= x + dxs[d] < n: 
-                    path.append(forest[y+dys[d]][x+dxs[d]])
-                    recur()
-                    path.pop()
+            # 인덱스 에러 방지
+            if 0 <= ny < n and 0 <= nx < n:
+                # 상 하 좌 우 에 나보다 큰 값이 있는지 확인
+                if forest[y][x] < forest[ny][nx]:
+                    # 방문한 적 있으면 pass
+                    if visited[ny][nx]:
+                        continue
+                    queue.append((ny, nx))
+                    cnt += 1
 
+    return cnt
 
 
 
 n = int(input())
 forest = [list(map(int, input().split())) for _ in range(n)]
-visited = [[0] * n for _ in range(n)]
-path = []
-recur()
 
+result = 0
+for i in range(n):
+    for j in range(n):
+        a = bfs(i, j)
+        result = max(a, result)
+
+print(result)
