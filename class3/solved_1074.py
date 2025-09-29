a5d5a96d320513a1028ef7dec22c6ffde31d99b6
hyuding <qo0723@naver.com>
2025-09-29 17:49:25 +0900

    solved

--- class3/solved_1074.py
+++ class3/solved_1074.py
@@ -4,44 +4,27 @@ sys.setrecursionlimit(10**6)
 N, r, c = map(int, input().split())
 
 
-a = r // 4 
-b = r % 4 
-aa = c // 4 
-bb = c % 4
+# 2 n-1 로 4 등분힘 > 4등분의 끝을 만나면 다음으로 넘어감
 
-if N > 2:
-    result = (((2 ** N) // 4) * 16 * a) - 1 + (aa * 16 ) + ((b-1) * 4 ) + bb
-else:
-    result = (((2 ** N) // 4) * 8 * a) - 1 + (aa * 8 ) + ((b-1) * 2 ) + bb
+#왼위, 오위, 왼아, 오아
+dys = [0, 0, 1, 1]
+dxs = [0, 1, 0, 1]
+memo = [[0] * (2 ** (N-1)) for _ in range(2 ** (N-1))]
 
-print(result)
+y = x = 0
+cnt = 0
+ 
+for i in range(y, 2 ** (N-1), 2):
+    for j in range(x, 2 ** (N-1), 2):
 
-# 왼위, 오위, 왼아, 오아
-# dys = [0, 0, 1, 1]
-# dxs = [0, 1, 0, 1]
+        for d in range(4):
+            dy = i + dys[d]
+            dx = j + dxs[d]
 
+            if dy == r and dx == c:
+                continue
 
-# def recur(y, x):
-#     global cnt
-#     if x == 2 ** N:
-#         x = 0
-#         y += 4
-
-#     for i in range(y, y + 4, 2):
-#         for j in range(x, x + 4, 2):
-
-#             for d in range(4):
-#                 dy = i + dys[d]
-#                 dx = j + dxs[d]
-
-#                 if dy == r and dx == c:
-#                     return 
-
-#                 cnt += 1
-        
-#     recur(y, x + 4)
-
+            memo[dy][dx] = cnt 
+            cnt += 1
             
-# cnt = 0
-# recur(0, 0)
-# print(cnt)
+print(memo)
