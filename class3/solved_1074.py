39dc64773798be8f2d007bf7976c63f0a1f18cd9
hyuding <qo0723@naver.com>
2025-10-01 17:35:12 +0900

    .

--- class3/solved_1074.py
+++ class3/solved_1074.py
@@ -4,29 +4,30 @@ sys.stdin = open('input.txt')
 N, r, c = map(int, input().split())
 
 
-# 2 n-1 로 4 등분힘 > 4등분의 끝을 만나면 다음으로 넘어감
-# 몇사분면에 있는지?
-# 그 사분면 어디인지
+def search_part(n, r, c):
+    if n == 0:
+        return 0
 
-#왼위, 오위, 왼아, 오아
-dys = [0, 0, 1, 1]
-dxs = [0, 1, 0, 1]
-memo = [[0] * (2 ** (N-1)) for _ in range(2 ** (N-1))]
+    half = 2 ** (N-1)
+    size = half * half
 
-y = x = 0
-cnt = 0
- 
-for i in range(y, 2 ** (N-1), 2):
-    for j in range(x, 2 ** (N-1), 2):
+    # 4 사분면
+    if r >= half and c >= half:
+        return size * 3 + search_part(n-1, r, c)
 
-        for d in range(4):
-            dy = i + dys[d]
-            dx = j + dxs[d]
+    # 3사분면
+    if r >= half and c < half:
+        return size * 2 + search_part(n-1, r, c)
+    
+    # 2사분면
+    if r < half and c >= half:
+        return size * 1 + search_part(n-1, r, c)
+    
+    # 1사분면
+    if r < half and c < half:
+        return search_part(n-1, r, c)
 
-            if dy == r and dx == c:
-                continue
 
-            memo[dy][dx] = cnt 
-            cnt += 1
-            
-print(memo)
+result = search_part(N, r, c)
+print(result)
+
