7f58f2f78c122ccc592edf3f039eb7e650335624
hyunding <qo0723@naver.com>
2025-10-01 23:29:02 +0900

    solved

--- class3/solved_1074.py
+++ class3/solved_1074.py
@@ -1,5 +1,6 @@
 import sys
-sys.stdin = open('input.txt')
+# sys.stdin = open('input.txt')
+sys.setrecursionlimit(10**6)
 
 N, r, c = map(int, input().split())
 
@@ -8,20 +9,20 @@ def search_part(n, r, c):
     if n == 0:
         return 0
 
-    half = 2 ** (N-1)
+    half = 2 ** (n-1)
     size = half * half
 
     # 4 사분면
     if r >= half and c >= half:
-        return size * 3 + search_part(n-1, r, c)
+        return size * 3 + search_part(n-1, r-half, c-half)
 
     # 3사분면
     if r >= half and c < half:
-        return size * 2 + search_part(n-1, r, c)
+        return size * 2 + search_part(n-1, r-half, c)
     
     # 2사분면
     if r < half and c >= half:
-        return size * 1 + search_part(n-1, r, c)
+        return size * 1 + search_part(n-1, r, c-half)
     
     # 1사분면
     if r < half and c < half:
