221730bd60250adbffcc2d3df543cfe2ce499cd5
hyunding <qo0723@naver.com>
2025-11-10 10:25:21 +0900

    solved

--- class3/solved_10026.py
+++ class3/solved_10026.py
@@ -1,38 +1,77 @@
 import sys
+from collections import deque
 sys.stdin = open('input.txt')
 
+def bfs(y, x):
+    global cnt
+    q = deque()
+    q.append((y, x))
+    visited[y][x] = cnt
+    while q:
+        y, x = q.popleft()
 
-def bfs():
-    area = 1
-    for y in range(1, N):
-        for x in range(N):
-            next_x = x + 1
+        for d in range(4):
+            dy = y + dys[d]
+            dx = x + dxs[d]
 
-            if 0 <= next_x < N: 
-                if colors[y][x] == colors[y][x+1] or colors[y][x] == colors[y-1][x]:
-                    visited[y][x] = area
-                
-                else:
-                    area += 1
-                    visited[y][x] = area
+            if 0 <= dy < N and 0 <= dx < N and visited[dy][dx] == 0:
+                if colors[y][x] == colors[dy][dx]:
+                    visited[dy][dx] = cnt
+                    q.append((dy, dx))
 
-            
-        for v in visited:
-            print(*v)
-        print('-'*15)
-                
+        # for v in visited:
+        #     print(*v)
+        # print('-' * 15)
 
+def bfs2(y, x):
+    global cnt2
+    q = deque()
+    q.append((y, x))
+    visited2[y][x] = cnt2
+    while q:
+        y, x = q.popleft()
+
+        for d in range(4):
+            dy = y + dys[d]
+            dx = x + dxs[d]
+
+            if 0 <= dy < N and 0 <= dx < N and visited2[dy][dx] == 0:
+                if colors[y][x] == 'B' and colors[dy][dx] == 'B':
+                    visited2[dy][dx] = cnt2
+                    q.append((dy, dx))
+
+                if colors[y][x] in ['R', 'G'] and colors[dy][dx] in ['R', 'G']:
+                    visited2[dy][dx] = cnt2
+                    q.append((dy, dx))
+
+        # for v in visited2:
+        #     print(*v)
+        # print('-' * 15)
 
 N = int(input())
 colors = [list(input()) for _ in range(N)]
 
-# 북 동 남 서
-dys = [-1, 0, 1, 0]
-dxs = [0, 1, 0, -1]
+# 서 북 동 남
+dys = [0, -1, 0, 1]
+dxs = [-1, 0, 1, 0]
 
 visited = [[0] * N for _ in range(N)]
+visited2 = [[0] * N for _ in range(N)]
+cnt = 0
+for y in range(N):
+    for x in range(N):
+        if visited[y][x] == 0:
+            cnt += 1
+            bfs(y, x)
 
-bfs()
+cnt2 = 0
+for y in range(N):
+    for x in range(N):
+        if visited2[y][x] == 0:
+            cnt2 += 1
+            bfs2(y, x)
+
+print(cnt, cnt2)
 
 
 # import sys
